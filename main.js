0f02adafc7f59f75aa4c62de49ed768c04454ee5
99sami <s3851083@student.rmit.edu.au>
2026-04-17 13:47:32 +1000

    first edit
    added my assets in

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // find our elements
 const stageContainer = document.getElementById("stage-container");
-const circleButton = document.getElementById("circle-button");
 
 let stageContainerWidth = stageContainer.offsetWidth;
 // console.log(stageContainerWidth);
@@ -8,10 +7,7 @@ let stageContainerWidth = stageContainer.offsetWidth;
 let stageContainerHeight = stageContainer.offsetHeight;
 // console.log(stageContainerHeight)
 
-// set default circle colour
-let circleColour = "red";
-
-// create the konca stage
+// create the konva stage
 const stage = new Konva.Stage({
   container: "konva-stage",
   width: stageContainerWidth,
@@ -22,3 +18,118 @@ const firstLayer = new Konva.Layer();
 
 // add the layer to our stage
 stage.add(firstLayer);
+
+// adding my rectangle
+const rect = new Konva.Rect({
+  x: 0,
+  y: 0,
+  width: 450,
+  height: 2000,
+  fill: "black",
+  // draggable: true
+  stroke: "pink",
+});
+
+// add rectangle to layer
+firstLayer.add(rect);
+
+// draw the layer
+firstLayer.draw();
+
+// Adding my flower pngs
+
+// const imageObj = new Image();
+
+// imageObj.onload = function () {
+//   const flower = new Konva.Image({
+//     x: 2,
+//     y: 2,
+//     image: imageObj,
+//     width: 120,
+//     height: 120,
+//     draggable: true
+//   });
+
+//   firstLayer.add(flower);
+//   firstLayer.draw();
+// };
+
+// imageObj.src = "assets/Flower1.png";
+
+// Adding images using an array due to the large quantity of files
+
+const flowers = [
+  "assets/Flower1.png",
+  "assets/Flower2.png",
+  "assets/Flower3.png",
+  "assets/Flower4.png",
+  "assets/Flower5.png",
+  "assets/Flower6.png",
+  "assets/Flower7.png",
+  "assets/Flower8.png",
+  "assets/Flower9.png",
+  "assets/Flower10.png",
+  "assets/Flower11.png",
+  "assets/Flower12.png",
+  "assets/Flower13.png",
+  "assets/Flower14.png",
+  "assets/Flower15.png",
+  "assets/Flower16.png",
+  "assets/Flower17.png",
+  "assets/Flower18.png",
+  "assets/Flower19.png",
+  "assets/Flower20.png",
+  "assets/Flower21.png",
+  "assets/Bouquet1.png",
+  "assets/Bouquet2.png",
+  "assets/Bouquet3.png",
+  "assets/Stem1.png",
+  "assets/Bow1.png",
+  "assets/Bow2.png",
+  "assets/Bow3.png",
+  "assets/Bow4.png",
+  "assets/Bow6.png",
+  "assets/Bow7.png",
+  "assets/Lace1.png",
+  "assets/Lace2.png",
+  "assets/Star1.png",
+  "assets/Star2.png",
+];
+
+flowers.forEach((path, index) => {
+  const imageObj = new Image();
+
+  imageObj.onload = function () {
+    const flower = new Konva.Image({
+      x: 20 + (index % 3) * 130,
+      y: 20 + Math.floor(index / 3) * 130,
+      image: imageObj,
+      width: 115,
+      height: 115,
+      draggable: true,
+    });
+
+    firstLayer.add(flower);
+    firstLayer.draw();
+  };
+
+  imageObj.src = path;
+
+  //   firstLayer.add(flower);
+});
+
+// Drag and Drop Function
+
+// add cursor styling
+// yoda.on('mouseover', function () {
+//     document.body.style.cursor = 'pointer';
+//   });
+//   yoda.on('mouseout', function () {
+//     document.body.style.cursor = 'default';
+//   });
+
+//   layer.add(yoda);
+// };
+// imageObj.src = flowers;
+
+// stage.add(layer);
